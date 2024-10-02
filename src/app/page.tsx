93564a2fae03545db067263216d4d2024b62a339
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-10-02 14:49:35 +0530

    pushing initial files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+  title : "PharmaVerse : a leading research platform for drug discovery",
+  description : "this is a description for PharmaVerse"
+}
 
 export default function Home() {
   return (
--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React , {useState , useLayoutEffect} from "react"
 import Sidebar from "@/components/Sidebar";
-
+import Header from "@/components/Header";
 export default function DefaultLayout({
     children ,
 }: {
@@ -16,7 +16,7 @@ export default function DefaultLayout({
         <div className="flex">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
             <div className="relative flex flex-1 flex-col lg:ml-72.5">
-                 {/**header */}
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
                  <main>
                     <div className="mx-auto max-w-screen-2xl p-4 dark:bg-[#121212] md:p-6 2xl:p-10">
                    {children}
